86a9b868a085974114ae2e4dfea68e40d6d92627
matthewstclaire <matthew.crnich@gmail.com>
2021-01-25 16:20:20 -0700

    App Complete

--- script.js
+++ script.js
@@ -9,18 +9,31 @@ const diceEl = document.querySelector(".dice");
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
-
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+  diceEl.classList.add("hidden");
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -29,6 +42,8 @@ const switchPlayer = function () {
   player0El.classList.toggle("player--active");
   player1El.classList.toggle("player--active");
 };
+
+init();
 
 //rolling dice functionality
 btnRoll.addEventListener("click", function (e) {
@@ -55,7 +70,6 @@ btnHold.addEventListener("click", function (e) {
     //scores[1] = scores[1] + currentScore
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    switchPlayer();
 
     if (scores[activePlayer] >= 10) {
       playing = false;
@@ -65,6 +79,7 @@ btnHold.addEventListener("click", function (e) {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove("player--active");
+      document.getElementById(`name--${activePlayer}`).textContent = "Winner";
     } else {
       switchPlayer();
     }
@@ -72,6 +87,8 @@ btnHold.addEventListener("click", function (e) {
 });
 
 //new game functionality
-btnNew.addEventListener("click", function (e) {
-  // body
+btnNew.addEventListener("click", function () {
+  init();
+  document.getElementById(`name--0`).textContent = "Player 1";
+  document.getElementById(`name--1`).textContent = "Player 2";
 });
