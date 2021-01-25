a3f33ee60e0770c27f400b59cd09f4df8a1ab7ee
matthewstclaire <matthew.crnich@gmail.com>
2021-01-25 15:08:53 -0700

    game logic complete

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
 "use strict";
+
+//selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
+//rolling dice functionality
+btnRoll.addEventListener("click", function (e) {
+  const diceRoll = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${diceRoll}.png`;
+
+  if (diceRoll !== 1) {
+    currentScore += diceRoll;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
+  } else {
+    switchPlayer();
+  }
+});
+
+//hold turn functionality
+btnHold.addEventListener("click", function (e) {
+  scores[activePlayer] += currentScore;
+  //scores[1] = scores[1] + currentScore
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  switchPlayer();
+  if (currentScore >= 100) {
+  }
+});
+
+//new game functionality
+btnNew.addEventListener("click", function (e) {
+  // body
+});
